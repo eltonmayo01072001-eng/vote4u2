a8301cfe77b316992682a47f739e5c89e4e01ab2
Raunaq2102001 <ralam2102001@gmail.com>
2025-09-06 08:45:41 +0530

    Fix backend API for Vercel deployment with CORS and MongoDB

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,7 +1,7 @@
 // mongodb.js
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = "mongodb+srv://eltonmayo01072001:Trip2%23ell@cluster0.84fj99m.mongodb.net/voting";
+const uri = "mongodb+srv://eltonmayo01072001:Trip2%23ell@cluster0.84fj99m.mongodb.net/voting?retryWrites=true&w=majority&appName=Cluster0";
 if (!uri) throw new Error("Add MONGODB_URI to environment variables");
 
 let client;
